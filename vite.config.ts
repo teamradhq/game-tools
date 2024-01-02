ae8b3427887a7a41270d497c7825ef44c1bf69f2
Paul Beynon <paul@beynon.app>
2024-01-02 15:32:56 +1100

    FEATURE: setup for debugging

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [react()],
   resolve: {
     alias: {
