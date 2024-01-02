3ac202a305e3456a946561593100b064f395e4df
Paul Beynon <paul@beynon.app>
2024-01-02 20:49:40 +1100

    REFACTOR: extract top-level styles.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,4 @@
-import '@mantine/core/styles.css';
-import '@mantine/carousel/styles.css';
-import '@mantine/code-highlight/styles.css';
-import '@mantine/dates/styles.css';
-import '@mantine/dropzone/styles.css';
-import '@mantine/notifications/styles.css';
-import '@mantine/nprogress/styles.css';
-import '@mantine/spotlight/styles.css';
-import '@mantine/tiptap/styles.css';
+import './styles';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -14,10 +6,9 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { MantineProvider } from '@mantine/core';
 
-import './index.css';
+import { RouterProvider } from './router';
 import { store } from './store';
-import { theme } from '@src/theme.ts';
-import { RouterProvider } from '@src/router';
+import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ src/styles.ts
@@ -0,0 +1,11 @@
+import '@mantine/core/styles.css';
+import '@mantine/carousel/styles.css';
+import '@mantine/code-highlight/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/dropzone/styles.css';
+import '@mantine/notifications/styles.css';
+import '@mantine/nprogress/styles.css';
+import '@mantine/spotlight/styles.css';
+import '@mantine/tiptap/styles.css';
+
+import './index.css';
