3fa7514cdfd46bd7adc63d62377202dcd37528c2
Paul Beynon <paul@beynon.app>
2024-01-03 16:32:52 +1100

    FEATURE: mock up a layout for scoreboard.

--- cypress/e2e/scoreboard.cy.ts
+++ cypress/e2e/scoreboard.cy.ts
@@ -6,4 +6,25 @@ describe('game scoreboard', () => {
   it('should link to home page', () => {
     cy.hasHomePageLink();
   });
+
+  it('should list players', () => {
+    cy.contains('Players');
+
+    Cypress._.times(5, (i) => {
+      cy.get('[data-testid="player-name"]')
+        .eq(i - 1)
+        .should('exist');
+    });
+
+    cy.contains('Edit Players');
+  });
+
+  it('should display the scoreboard', () => {
+    cy.contains('Scores');
+  });
+
+  it('should display game summary', () => {
+    cy.contains('Summary');
+    cy.contains('0:10');
+  });
 });
--- src/router/ScoreboardPage.tsx
+++ src/router/ScoreboardPage.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
 
 import { Layout } from '@src/Layout.tsx';
+import { Button, Flex, List, Paper, Title, VisuallyHidden } from '@mantine/core';
+import { IconPencil } from '@tabler/icons-react';
+import { HourGlass } from '@src/components/Timer/HourGlass.tsx';
 
 export function ScoreboardPage(): React.JSX.Element {
-  return <Layout title={'Scoreboard'}>Scoreboard Page!</Layout>;
+  return (
+    <Layout title={'Scoreboard'}>
+      <Flex align="stretch" style={{ minHeight: '70svh' }}>
+        <Paper w="20%" style={{ position: 'relative' }}>
+          <Title order={3} mb="sm">
+            Players
+          </Title>
+          <List type="ordered">
+            <List.Item data-testid="player-name">Player 1</List.Item>
+            <List.Item data-testid="player-name">Player 2</List.Item>
+            <List.Item data-testid="player-name">Player 3</List.Item>
+            <List.Item data-testid="player-name">Player 4</List.Item>
+          </List>
+          <Button
+            size="xs"
+            px="xs"
+            color="green"
+            title="Edit Players"
+            style={{ position: 'absolute', top: '1rem', right: '1rem' }}
+          >
+            <IconPencil size="20" />
+            <VisuallyHidden>Edit Players</VisuallyHidden>
+          </Button>
+        </Paper>
+        <Paper w="50%">
+          <Title order={3} mb="sm">
+            Scores
+          </Title>
+        </Paper>
+        <Paper w="30%">
+          <Title order={3} mb="sm">
+            Summary
+          </Title>
+          <HourGlass />
+        </Paper>
+      </Flex>
+    </Layout>
+  );
 }
